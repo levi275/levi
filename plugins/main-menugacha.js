afe0c39ed83597eea41c6a27e572b5fe4d9cd971
Dioneibi-rip <fandubdreams@gmail.com>
2025-11-19 23:41:49 -0400

    Actualizar main-menugacha.js

--- plugins/main-menugacha.js
+++ plugins/main-menugacha.js
@@ -22,14 +22,14 @@ let handler = async (m, { conn }) => {
   `.trim();
 
   await conn.sendMessage(m.chat, {
-    image: { url: 'https://files.catbox.moe/61219t.png' },
+    image: { url: 'https://files.catbox.moe/jau272.jpeg' },
     caption: texto,
     contextInfo: {
       mentionedJid: [m.sender],
       externalAdReply: {
         title: 'Menu de comandos gacha 🍡',
         body: 'colecciona todos los personajes que puedas',
-        thumbnailUrl: 'https://files.catbox.moe/hdr7oh.jpg',
+        thumbnail: icons,
         mediaType: 1,
         renderLargerThumbnail: false,
         showAdAttribution: true,
