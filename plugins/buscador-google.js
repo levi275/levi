02d8b654bef80c271393f18bf71929b94f1d8a2d
Dioneibi-rip <fandubdreams@gmail.com>
2025-11-26 12:40:28 -0400

    Actualizar buscador-google.js

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -1,38 +1,28 @@
-import fetch from 'node-fetch';
-
-let handler = async (m, { text }) => {
-  if (!text) {
-    m.reply(`${emoji} Por favor, proporciona el termino de búsqueda que deseas realizar a *Google*.`);
-    return;
-  }
-
-  const apiUrl = `https://delirius-apiofc.vercel.app/search/googlesearch?query=${encodeURIComponent(text)}`;
-
-  try {
-    const response = await fetch(apiUrl);
-    const result = await response.json();
-
-    if (!result.status) {
-      m.reply('Error al realizar la búsqueda.');
-      return;
-    }
-
-    let replyMessage = `${emoji2} Resultados de la búsqueda:\n\n`;
-    result.data.slice(0, 1).forEach((item, index) => {
-      replyMessage += `☁️ *${index + 1}. ${item.title}*\n`;
-      replyMessage += `📰 *${item.description}*\n`;
-      replyMessage += `🔗 URL: ${item.url}`;
-    });
-
-m.react('✅')
-
-    m.reply(replyMessage);
-  } catch (error) {
-    console.error(`${msm} Error al realizar la solicitud a la API:`, error);
-    m.reply(`${msm} Ocurrió un error al obtener los resultados.`);
-  }
-};
-
-handler.command = ['google'];
-
-export default handler;
+import {googleIt} from '@bochilteam/scraper'
+import google from 'google-it'
+import axios from 'axios'
+let handler = async (m, { conn, command, args, usedPrefix }) => {
+const fetch = (await import('node-fetch')).default;
+const text = args.join` `
+if (!text) return conn.reply(m.chat, '🍟 Ingresa lo que deseas buscar en Google.', m, rcanal)
+conn.reply(m.chat, `🚩 Buscando Su Información...`, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
+const url = 'https://google.com/search?q=' + encodeURIComponent(text)
+google({'query': text}).then(res => {
+let teks = `🍟 *Resultado de* : ${text}\n\n`
+for (let g of res) {
+teks += `🐢 *Titulo ∙* ${g.title}\n🚩 *Info ∙* ${g.snippet}\n🔗 *Url ∙* ${g.link}\n\n`
+}
+conn.reply(m.chat, teks, m, rcanal)
+})
+}
+handler.help = ['google <búsqueda>']
+handler.tags = ['buscador']
+handler.command = ['google']
+handler.group = true;
+handler.register = true
+export default handler
