b6c9fd2313f7845e9bed1c1fdae8fd40f1948ccc
Dioneibi-rip <fandubdreams@gmail.com>
2026-01-06 18:30:34 -0400

    Actualizar anime-kiss.js

--- plugins/anime-kiss.js
+++ plugins/anime-kiss.js
@@ -54,7 +54,7 @@ const kissGifs=[
 'https://media.tenor.com/SZ8-4vDwi6cAAAAM/miyamura-hori.gif',
 'https://media.tenor.com/b7DWF8ecBkIAAAAM/kiss-anime-anime.gif',
 'https://media.tenor.com/g92jdEmFrn0AAAAM/anime-kiss-anime.gif',
-'https://media.tenor.com/6o7pJb2d2fUAAAAC/anime-kiss.gif',
+'https://media.tenor.com/ZDqsYLDQzIUAAAAM/shirayuki-zen-kiss-anime.gif',
 'https://media.tenor.com/6o7pJb2d2fUAAAAC/anime-kiss.gif',
 'https://media.tenor.com/6o7pJb2d2fUAAAAC/anime-kiss.gif',
 'https://media.tenor.com/6o7pJb2d2fUAAAAC/anime-kiss.gif',
