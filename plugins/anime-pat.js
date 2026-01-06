177fc22d0a9fcef823c987a0bb02ad31ec5fed8a
Dioneibi-rip <fandubdreams@gmail.com>
2026-01-06 17:07:04 -0400

    Actualizar anime-pat.js

--- plugins/anime-pat.js
+++ plugins/anime-pat.js
@@ -29,7 +29,7 @@ let handler=async(m,{conn})=>{
 const patGifs=[
 'https://i.pinimg.com/originals/63/82/a7/6382a71ef92eb583e2218af754163c4d.gif',
 'https://i.pinimg.com/originals/e3/e2/58/e3e2588fbae9422f2bd4813c324b1298.gif',
-'https://media.tenor.com/9JtY9C2F8YkAAAAC/anime-pat-head.gif',
+'https://i.pinimg.com/originals/27/da/ce/27dace4b6aec7de261ddb5d9444e716a.gif',
 'https://media.tenor.com/3Fq8g5cF8ZQAAAAC/anime-pat.gif',
 'https://media.tenor.com/0z4K8pF5ySAAAAAC/anime-pat.gif'
 ]
