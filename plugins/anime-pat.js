21ac2b3cad88bf11a9a0038941e20badafc66cf9
Dioneibi-rip <fandubdreams@gmail.com>
2026-01-06 17:18:26 -0400

    Actualizar anime-pat.js

--- plugins/anime-pat.js
+++ plugins/anime-pat.js
@@ -39,7 +39,7 @@ const patGifs=[
 'https://media.tenor.com/Dbg-7wAaiJwAAAAM/aharen-aharen-san.gif', 
 'https://media.tenor.com/CIF_Pa3yepwAAAAM/rika-higurashi.gif', 
 'https://media.tenor.com/Zm71HaIh7wwAAAAM/pat-pat.gif', 
-'https://i.pinimg.com/originals/73/08/9d/73089d4e4b3f570ef3d5a7ca9f68e622.gif', 
+'https://media.tenor.com/YMRmKEdwZCgAAAAM/anime-hug-anime.gif', 
 'https://i.pinimg.com/originals/73/08/9d/73089d4e4b3f570ef3d5a7ca9f68e622.gif', 
 'https://i.pinimg.com/originals/73/08/9d/73089d4e4b3f570ef3d5a7ca9f68e622.gif'
 ]
