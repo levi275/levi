c030adcd301a44038681b95df19f6ee06c49c6d3
Dioneibi-rip <fandubdreams@gmail.com>
2026-01-06 17:16:20 -0400

    Actualizar anime-pat.js

--- plugins/anime-pat.js
+++ plugins/anime-pat.js
@@ -35,7 +35,7 @@ const patGifs=[
 'https://media.tenor.com/PkWttKcH1xMAAAAM/kobayashi-dragon.gif', 
 'https://media.tenor.com/8FOQORmaLNoAAAAM/shiro-anime.gif', 
 'https://media.tenor.com/wLqFGYigJuIAAAAM/mai-sakurajima.gif', 
-'https://i.pinimg.com/originals/73/08/9d/73089d4e4b3f570ef3d5a7ca9f68e622.gif', 
+'https://media.tenor.com/fro6pl7src0AAAAM/hugtrip.gif', 
 'https://i.pinimg.com/originals/73/08/9d/73089d4e4b3f570ef3d5a7ca9f68e622.gif', 
 'https://i.pinimg.com/originals/73/08/9d/73089d4e4b3f570ef3d5a7ca9f68e622.gif', 
 'https://i.pinimg.com/originals/73/08/9d/73089d4e4b3f570ef3d5a7ca9f68e622.gif', 
