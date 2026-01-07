af00f0752288e0f71cc4a81cba0ff545ba4436ce
Dioneibi-rip <fandubdreams@gmail.com>
2026-01-06 20:07:03 -0400

    Actualizar anime-sleep.js

--- plugins/anime-sleep.js
+++ plugins/anime-sleep.js
@@ -38,7 +38,7 @@ const sleepGifs=[
 'https://i.pinimg.com/originals/52/12/c6/5212c66558e9eb4609bd8038e4794274.gif',
 'https://i.pinimg.com/originals/c3/bc/10/c3bc10f31eca300f1d5ea035cf32df43.gif', 
 'https://i.pinimg.com/originals/60/24/7b/60247b6a7f2b1e2e6b832eaee550cc05.gif',
-'https://telegra.ph/file/6b8e6cc26de052d4018ba.mp4',
+'https://i.pinimg.com/originals/8d/74/d0/8d74d090209a9f763f7a4331a4c1b693.gif',
 'https://telegra.ph/file/6b8e6cc26de052d4018ba.mp4',
 'https://telegra.ph/file/6b8e6cc26de052d4018ba.mp4',
 'https://telegra.ph/file/6b8e6cc26de052d4018ba.mp4',
