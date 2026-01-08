b1e3f48ad6933ffebd978782c2468ae0e0d1b7f9
Dioneibi-rip <fandubdreams@gmail.com>
2026-01-08 19:09:59 -0400

    Actualizar anime-pout.js

--- plugins/anime-pout.js
+++ plugins/anime-pout.js
@@ -61,7 +61,7 @@ const poutGifs=[
 'https://media.tenor.com/_22_7mYKy5EAAAAM/bocchi-bocchi-the-rock.gif',
 'https://media.tenor.com/iNu8LXx2ECgAAAAM/senko-poute-hmph.gif',
 'https://media.tenor.com/Mf_UetbqAMIAAAAM/oshi-no-ko-onk.gif',
-'https://telegra.ph/file/ec2d25e70b165a19e7ef7.mp4',
+'https://media.tenor.com/Ff7mEcLQ8XkAAAAm/pouting-frieren.webp',
 'https://telegra.ph/file/ec2d25e70b165a19e7ef7.mp4',
 'https://telegra.ph/file/ec2d25e70b165a19e7ef7.mp4',
 'https://telegra.ph/file/ec2d25e70b165a19e7ef7.mp4',
