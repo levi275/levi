4a52fe548bf551e5af61993757de93bb627fe6a2
Dioneibi-rip <fandubdreams@gmail.com>
2026-01-08 02:32:38 -0400

    Actualizar anime-scared.js

--- plugins/anime-scared.js
+++ plugins/anime-scared.js
@@ -51,7 +51,7 @@ const scaredGifs=[
 'https://media.tenor.com/2FPcjP8vlTwAAAAM/saitama-one-punch-man.gif',
 'https://media.tenor.com/-QSwbUkUu8cAAAAM/scared-terrified.gif',
 'https://media.tenor.com/VgZoIGF_n9wAAAAM/scream-screaming.gif',
-'https://telegra.ph/file/dcde646a58d8e9bf44867.mp4',
+'https://media.tenor.com/VtJHsA9slmAAAAA1/mahito-jjk-jjk-s2.webp',
 'https://telegra.ph/file/dcde646a58d8e9bf44867.mp4',
 'https://telegra.ph/file/dcde646a58d8e9bf44867.mp4',
 'https://telegra.ph/file/dcde646a58d8e9bf44867.mp4',
