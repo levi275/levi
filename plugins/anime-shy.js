82741e293cacacaad74442b75e1ff4ea53237d96
Dioneibi-rip <fandubdreams@gmail.com>
2026-01-08 01:34:29 -0400

    Actualizar anime-shy.js

--- plugins/anime-shy.js
+++ plugins/anime-shy.js
@@ -1,55 +1,72 @@
-//Codígo creado por Destroy wa.me/584120346669
+import axios from 'axios'
+import fs from 'fs'
+import path from 'path'
+import { spawn } from 'child_process'
+import { tmpdir } from 'os'
 
-import fs from 'fs';
-import path from 'path';
-
-let handler = async (m, { conn, usedPrefix }) => {
-    let who;
-
-    if (m.mentionedJid.length > 0) {
-        who = m.mentionedJid[0];
-    } else if (m.quoted) {
-        who = m.quoted.sender;
-    } else {
-        who = m.sender;
-    }
-
-    let name = conn.getName(who);
-    let name2 = conn.getName(m.sender);
-    m.react('😛');
-
-    let str;
-    if (m.mentionedJid.length > 0) {
-        str = `\`${name2}\` *está  tímid﹫ por* \`${name || who}\`.`;
-    } else if (m.quoted) {
-        str = `\`${name2}\` *está  tímid﹫ por* \`${name || who}\`.`;
-    } else {
-        str = `\`${name2}\` *es muy timid﹫*.`.trim();
-    }
-    
-    if (m.isGroup) {
-        let pp = 'https://telegra.ph/file/a9ccfa5013d58fad2e677.mp4'; 
-        let pp2 = 'https://telegra.ph/file/2cd355afa143095b97890.mp4'; 
-        let pp3 = 'https://telegra.ph/file/362c8566dc9367a5a473d.mp4';
-        let pp4 = 'https://telegra.ph/file/362c8566dc9367a5a473d.mp4';
-        let pp5 = 'https://telegra.ph/file/4f9323ca22e126b9d275c.mp4';
-        let pp6 = 'https://telegra.ph/file/51b688e0c5295bc37ca92.mp4';
-        let pp7 = 'https://telegra.ph/file/dfe74d7eee02c170f6f55.mp4';
-        let pp8 = 'https://telegra.ph/file/697719af0e6f3baec4b2f.mp4';
-        let pp9 = 'https://telegra.ph/file/89e1e1e44010975268b38.mp4';
-        let pp10 = 'https://telegra.ph/file/654313ad5a3e8b43fc535.mp4';
-        
-        const videos = [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10];
-        const video = videos[Math.floor(Math.random() * videos.length)];
-
-        let mentions = [who];
-        conn.sendMessage(m.chat, { video: { url: video }, gifPlayback: true, caption: str, mentions }, { quoted: m });
-    }
+function gifToMp4(gifBuffer){
+return new Promise((resolve,reject)=>{
+const tempGif=path.join(tmpdir(),`${Date.now()}.gif`)
+const tempMp4=path.join(tmpdir(),`${Date.now()}.mp4`)
+fs.writeFileSync(tempGif,gifBuffer)
+const ffmpeg=spawn('ffmpeg',['-y','-i',tempGif,'-c:v','libx264','-pix_fmt','yuv420p','-vf','scale=trunc(iw/2)*2:trunc(ih/2)*2','-movflags','+faststart',tempMp4])
+ffmpeg.on('close',code=>{
+fs.unlinkSync(tempGif)
+if(code===0){
+const mp4Buffer=fs.readFileSync(tempMp4)
+fs.unlinkSync(tempMp4)
+resolve(mp4Buffer)
+}else reject(new Error(`ffmpeg error ${code}`))
+})
+ffmpeg.on('error',err=>{
+fs.unlinkSync(tempGif)
+reject(err)
+})
+})
 }
 
-handler.help = ['shy/timida @tag'];
-handler.tags = ['anime'];
-handler.command = ['shy','timida'];
-handler.group = true;
+let handler=async(m,{conn})=>{
+let who=m.mentionedJid&&m.mentionedJid[0]?m.mentionedJid[0]:m.quoted?m.quoted.sender:m.sender
+let nameSender=conn.getName(m.sender)
+let nameTarget=conn.getName(who)
 
-export default handler;
+let caption=who===m.sender
+? `\`${nameSender}\` *es muy timid﹫.*`
+: `\`${nameSender}\` *está tímid﹫ por* \`${nameTarget}\`.`
+
+await m.react('😛')
+
+const shyGifs=[
+'https://telegra.ph/file/a9ccfa5013d58fad2e677.mp4',
+'https://telegra.ph/file/2cd355afa143095b97890.mp4',
+'https://telegra.ph/file/362c8566dc9367a5a473d.mp4',
+'https://telegra.ph/file/4f9323ca22e126b9d275c.mp4',
+'https://telegra.ph/file/51b688e0c5295bc37ca92.mp4',
+'https://telegra.ph/file/dfe74d7eee02c170f6f55.mp4',
+'https://telegra.ph/file/697719af0e6f3baec4b2f.mp4',
+'https://telegra.ph/file/89e1e1e44010975268b38.mp4',
+'https://telegra.ph/file/654313ad5a3e8b43fc535.mp4'
+]
+
+const randomGif=shyGifs[Math.floor(Math.random()*shyGifs.length)]
+
+try{
+const response=await axios({method:'get',url:randomGif,responseType:'arraybuffer',headers:{'User-Agent':'Mozilla/5.0','Referer':'https://google.com/'}})
+let buffer=Buffer.from(response.data)
+try{
+buffer=await gifToMp4(buffer)
+await conn.sendMessage(m.chat,{video:buffer,caption:caption,gifPlayback:true,mentions:[who,m.sender],mimetype:'video/mp4'},{quoted:m})
+}catch{
+throw new Error('conversion fail')
+}
+}catch{
+await conn.sendMessage(m.chat,{video:{url:randomGif},caption:caption,gifPlayback:true,mentions:[who,m.sender]},{quoted:m})
+}
+}
+
+handler.help=['shy','timida']
+handler.tags=['anime']
+handler.command=['shy','timida']
+handler.group=true
+
+export default handler
