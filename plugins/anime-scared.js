366c9d2ac046663c349f007f7b3b62823cf89b59
Dioneibi-rip <fandubdreams@gmail.com>
2026-01-08 02:34:47 -0400

    Actualizar anime-scared.js

--- plugins/anime-scared.js
+++ plugins/anime-scared.js
@@ -54,7 +54,7 @@ const scaredGifs=[
 'https://media.tenor.com/VtJHsA9slmAAAAA1/mahito-jjk-jjk-s2.webp',
 'https://media.tenor.com/RhyxCbENd6YAAAAM/umaru-chan-scared.gif',
 'https://media.tenor.com/FLiPfaiTRScAAAAm/terrified-panic.webp',
-'https://telegra.ph/file/dcde646a58d8e9bf44867.mp4',
+'https://media.tenor.com/nEh0yvlMrEgAAAAM/anime-scare.gif',
 'https://telegra.ph/file/dcde646a58d8e9bf44867.mp4',
 'https://telegra.ph/file/dcde646a58d8e9bf44867.mp4',
 'https://telegra.ph/file/dcde646a58d8e9bf44867.mp4',
