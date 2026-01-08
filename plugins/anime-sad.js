97bc946ec2e0d0997f94ff2ec55287b6db3ad783
Dioneibi-rip <fandubdreams@gmail.com>
2026-01-08 03:07:10 -0400

    Actualizar anime-sad.js

--- plugins/anime-sad.js
+++ plugins/anime-sad.js
@@ -61,7 +61,7 @@ const sadGifs=[
 'https://media.tenor.com/opUBGwM4o48AAAAM/ghoul.gif',
 'https://media.tenor.com/KMK8l8SqB28AAAAM/cute-anime-boy-sakura-haruka.gif',
 'https://media.tenor.com/q7cz_01J4zsAAAAM/maid-cute.gif',
-'https://telegra.ph/file/3e940fb5e2b2277dc754b.mp4',
+'https://media.tenor.com/Ifjf8kh9xi8AAAAM/last-tour-girl-sad-anime-melancholy.gif',
 'https://telegra.ph/file/3e940fb5e2b2277dc754b.mp4',
 'https://telegra.ph/file/3e940fb5e2b2277dc754b.mp4',
 'https://telegra.ph/file/3e940fb5e2b2277dc754b.mp4',
