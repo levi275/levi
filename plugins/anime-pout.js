ae438be4c35efd74a49160bb2c74477db84ca1e8
Dioneibi-rip <fandubdreams@gmail.com>
2026-01-08 19:13:04 -0400

    Actualizar anime-pout.js

--- plugins/anime-pout.js
+++ plugins/anime-pout.js
@@ -64,7 +64,7 @@ const poutGifs=[
 'https://media.tenor.com/Ff7mEcLQ8XkAAAAm/pouting-frieren.webp',
 'https://media.tenor.com/ob2MggREvn0AAAAM/angry-hmph.gif',
 'https://media.tenor.com/3EgO4ozQzp4AAAAM/anime-raphtalia.gif',
-'https://telegra.ph/file/ec2d25e70b165a19e7ef7.mp4',
+'https://media.tenor.com/Up7hRFmFY9AAAAAM/anime-sad-anime-pout.gif',
 'https://telegra.ph/file/ec2d25e70b165a19e7ef7.mp4',
 'https://telegra.ph/file/ec2d25e70b165a19e7ef7.mp4'
 ]
