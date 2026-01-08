531273eec89ac91c70ef691774e1995480f26e72
Dioneibi-rip <fandubdreams@gmail.com>
2026-01-08 01:25:44 -0400

    Actualizar anime-slap.js

--- plugins/anime-slap.js
+++ plugins/anime-slap.js
@@ -53,7 +53,7 @@ const slapGifs=[
 'https://media.tenor.com/cpWuWnOU64MAAAAM/bofetada.gif',
 'https://media.tenor.com/XiYuU9h44-AAAAAM/anime-slap-mad.gif',
 'https://media.tenor.com/E3OW-MYYum0AAAAM/no-angry.gif',
-'https://i.pinimg.com/originals/0c/09/ec/0c09ecfac6e0d93b894ec13fa900f9fa.gif',
+'https://media.tenor.com/8bSm0lI4_FUAAAAM/yuuri.gif',
 'https://i.pinimg.com/originals/0c/09/ec/0c09ecfac6e0d93b894ec13fa900f9fa.gif',
 'https://i.pinimg.com/originals/0c/09/ec/0c09ecfac6e0d93b894ec13fa900f9fa.gif',
 'https://i.pinimg.com/originals/0c/09/ec/0c09ecfac6e0d93b894ec13fa900f9fa.gif',
