7562a76be804965f9c508497f8f55592844894e3
Dioneibi-rip <fandubdreams@gmail.com>
2026-01-08 17:09:17 -0400

    Actualizar anime-run.js

--- plugins/anime-run.js
+++ plugins/anime-run.js
@@ -42,8 +42,19 @@ const runGifs=[
 'https://i.pinimg.com/originals/33/c9/9a/33c99abddaca08bafd75384d3ee7e677.gif',
 'https://i.pinimg.com/originals/e5/dd/c0/e5ddc029075f2f1ef6781835d144ea10.gif',
 'https://i.pinimg.com/originals/31/67/80/316780713631c15539e0ebddd7e8a572.gif',
-'https://qu.ax/qTAxM.mp4',
+'https://i.pinimg.com/originals/81/21/85/8121852ccbb18e7c70cb53fd81f27a54.gif',
 'https://qu.ax/oCYed.mp4',
+'https://qu.ax/OPMAT.mp4',
+'https://qu.ax/OPMAT.mp4',
+'https://qu.ax/OPMAT.mp4',
+'https://qu.ax/OPMAT.mp4',
+'https://qu.ax/OPMAT.mp4',
+'https://qu.ax/OPMAT.mp4',
+'https://qu.ax/OPMAT.mp4',
+'https://qu.ax/OPMAT.mp4',
+'https://qu.ax/OPMAT.mp4',
+'https://qu.ax/OPMAT.mp4',
+'https://qu.ax/OPMAT.mp4',
 'https://qu.ax/OPMAT.mp4'
 ]
 
