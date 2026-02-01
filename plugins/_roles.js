7c1635bd1509e30b40fcb8c6c559b9fa576421d7
Dioneibi-rip <fandubdreams@gmail.com>
2026-02-01 17:20:27 -0400

    Update _roles.js

--- plugins/_roles.js
+++ plugins/_roles.js
@@ -105,12 +105,23 @@ const roles = {
 }
 
 let handler = m => m
+
 handler.before = async function (m, { conn }) {
   let user = db.data.users[m.sender]
-  let level = user.level || 0
-  if (!user) return !0
-  let role = (Object.entries(roles).sort((a, b) => b[1] - a[1]).find(([, minLevel]) => level >= minLevel) || Object.entries(roles)[0])[0]
+
+  if (!user) return true
+
+  let level = user.level ?? 0
+
+  let role = (
+    Object.entries(roles)
+      .sort((a, b) => b[1] - a[1])
+      .find(([, minLevel]) => level >= minLevel)
+    || Object.entries(roles)[0]
+  )[0]
+
   user.role = role
-  return !0
+  return true
 }
+
 export default handler
