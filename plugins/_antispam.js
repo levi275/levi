ab421ff40512a674b39b7c4034ff9ecb57666375
Dioneibi-rip <fandubdreams@gmail.com>
2026-02-24 15:31:14 -0400

    perf: reduce per-message overhead in anti-spam and detect hooks

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -1,115 +1,94 @@
-const userSpamData = {}
-let handler = m => m
-handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner, isROwner, isPrems}) {
-const chat = global.db.data.chats[m.chat]
-const bot = global.db.data.settings[conn.user.jid] || {}
-if (!bot.antiSpam) return
-if (m.isGroup && chat.modoadmin) return  
-if (m.isGroup) {
-if (isOwner || isROwner || isAdmin || !isBotAdmin || isPrems) return
-}  
-let user = global.db.data.users[m.sender]
-const sender = m.sender
-const currentTime = new Date().getTime()
-const timeWindow = 5000 // tiempo límite 
-const messageLimit = 10 // cantidad de mensajes en dicho tiempo
+const userSpamData = Object.create(null)
 
-let time, time2, time3, mensaje, motive
-time = 30000 // 30 seg
-time2 = 60000 // 1 min
-time3 = 120000 // 2 min 
-
-if (!(sender in userSpamData)) {
-userSpamData[sender] = {
-lastMessageTime: currentTime,
-messageCount: 1, 
-antiBan: 0, 
-message: 0,
-message2: 0,
-message3: 0,
+const TIME_WINDOW_MS = 5000
+const MESSAGE_LIMIT = 10
+const PENALTY_RESET = {
+  1: 30000,
+  2: 60000,
+  3: 120000,
 }
-} else {
-const userData = userSpamData[sender]
-const timeDifference = currentTime - userData.lastMessageTime
 
-if (userData.antiBan === 1) {
-if (userData.message < 1) {
-userData.message++  
-motive = `✦ No hagas spam.`
-await conn.reply(m.chat, motive, m, { mentions: [m.sender] })  
-user.messageSpam = motive
-}} else if (userData.antiBan === 2) {
-if (userData.message2 < 1) {
-userData.message2++  
-motive =  `✦ No hagas spam...`
-await conn.reply(m.chat, motive, m, { mentions: [m.sender] })  
-user.messageSpam = motive
-}} else if (userData.antiBan === 3) {
-if (userData.message3 < 1) {
-userData.message3++  
-motive = `✦ Seras eliminado(a) por hacer spam.`
-await conn.reply(m.chat, motive, m, { mentions: [m.sender] }) 
-user.messageSpam = motive
-await conn.groupParticipantsUpdate(m.chat, [sender], 'remove')
-}}
+function resetUserSpamState(userData, user) {
+  userData.antiBan = 0
+  userData.messageCount = 1
+  userData.warnedLevel = 0
 
-if (timeDifference <= timeWindow) {
-userData.messageCount += 1
+  user.antispam = 0
+  user.messageSpam = 0
+  user.banned = false
+}
 
-if (userData.messageCount >= messageLimit) {
-const mention = `@${sender.split("@")[0]}`
-const warningMessage = `✦ *Mucho Spam*\n\n✐ 𝙐𝙨𝙪𝙖𝙧𝙞𝙤: ${mention}`
-if (userData.antiBan > 2) return
-await conn.reply(m.chat, warningMessage, m, { mentions: [m.sender] })  
-user.banned = true
-userData.antiBan++
-userData.messageCount = 1
+let handler = m => m
+handler.before = async function (m, { conn, isAdmin, isBotAdmin, isOwner, isROwner, isPrems }) {
+  const bot = global.db.data.settings[conn.user.jid] || {}
+  if (!bot.antiSpam) return
 
-if (userData.antiBan === 1) {
-setTimeout(() => {
-if (userData.antiBan === 1) {
-userData.antiBan = 0
-userData.message = 0
-userData.message2 = 0
-userData.message3 = 0
-user.antispam = 0
-motive = 0
-user.messageSpam = 0
-user.banned = false
-}}, time) 
+  if (!m.isGroup) return
 
-} else if (userData.antiBan === 2) {
-setTimeout(() => {
-if (userData.antiBan === 2) {
-userData.antiBan = 0
-userData.message = 0
-userData.message2 = 0
-userData.message3 = 0
-user.antispam = 0
-motive = 0
-user.messageSpam = 0
-user.banned = false
-}}, time2) 
+  const chat = global.db.data.chats[m.chat]
+  if (chat.modoadmin) return
 
-} else if (userData.antiBan === 3) {
-setTimeout(() => {
-if (userData.antiBan === 3) {
-userData.antiBan = 0
-userData.message = 0
-userData.message2 = 0
-userData.message3 = 0
-user.antispam = 0
-motive = 0
-user.messageSpam = 0
-user.banned = false
-}}, time3)
+  if (isOwner || isROwner || isAdmin || !isBotAdmin || isPrems) return
 
-}}
-} else {
-if (timeDifference >= 2000) {
-userData.messageCount = 1
-}}
-userData.lastMessageTime = currentTime
-}}
+  const user = global.db.data.users[m.sender]
+  const sender = m.sender
+  const now = Date.now()
+
+  const userData = userSpamData[sender] || (userSpamData[sender] = {
+    lastMessageTime: now,
+    messageCount: 1,
+    antiBan: 0,
+    warnedLevel: 0,
+  })
+
+  const timeDifference = now - userData.lastMessageTime
+
+  if (userData.antiBan > 0 && userData.warnedLevel !== userData.antiBan) {
+    userData.warnedLevel = userData.antiBan
+
+    const motive = userData.antiBan === 1
+      ? '✦ No hagas spam.'
+      : userData.antiBan === 2
+        ? '✦ No hagas spam...'
+        : '✦ Seras eliminado(a) por hacer spam.'
+
+    await conn.reply(m.chat, motive, m, { mentions: [sender] })
+    user.messageSpam = motive
+
+    if (userData.antiBan === 3) {
+      await conn.groupParticipantsUpdate(m.chat, [sender], 'remove')
+    }
+  }
+
+  if (timeDifference <= TIME_WINDOW_MS) {
+    userData.messageCount += 1
+
+    if (userData.messageCount >= MESSAGE_LIMIT) {
+      if (userData.antiBan > 2) {
+        userData.lastMessageTime = now
+        return
+      }
+
+      const mention = `@${sender.split('@')[0]}`
+      const warningMessage = `✦ *Mucho Spam*\n\n✐ 𝙐𝙨𝙪𝙖𝙧𝙞𝙤: ${mention}`
+      await conn.reply(m.chat, warningMessage, m, { mentions: [sender] })
+
+      user.banned = true
+      userData.antiBan += 1
+      userData.messageCount = 1
+
+      const currentLevel = userData.antiBan
+      setTimeout(() => {
+        if (userData.antiBan === currentLevel) {
+          resetUserSpamState(userData, user)
+        }
+      }, PENALTY_RESET[currentLevel])
+    }
+  } else if (timeDifference >= 2000) {
+    userData.messageCount = 1
+  }
+
+  userData.lastMessageTime = now
+}
 
 export default handler
--- plugins/_autodetect.js
+++ plugins/_autodetect.js
@@ -1,98 +1,155 @@
-import { WAMessageStubType } from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
-const styleText = (text) => {
-const map = {
+
+const STYLE_MAP = {
 'a': '𝘢', 'b': '𝘣', 'c': '𝘤', 'd': '𝘥', 'e': '𝘦', 'f': '𝘧', 'g': '𝘨', 'h': '𝘩', 'i': '𝘪', 'j': '𝘫', 'k': '𝘬', 'l': '𝘭', 'm': '𝘮', 'n': '𝘯', 'o': '𝘰', 'p': '𝘱', 'q': '𝘲', 'r': '𝘳', 's': '𝘴', 't': '𝘵', 'u': '𝘶', 'v': '𝘷', 'w': '𝘸', 'x': '𝘹', 'y': '𝘺', 'z': '𝘻',
 'A': '𝘼', 'B': '𝘽', 'C': '𝘾', 'D': '𝘿', 'E': '𝙀', 'F': '𝙁', 'G': '𝙂', 'H': '𝙃', 'I': '𝙄', 'J': '𝙅', 'K': '𝙆', 'L': '𝙇', 'M': '𝙈', 'N': '𝙉', 'O': '𝙊', 'P': '𝙋', 'Q': '𝙌', 'R': '𝙍', 'S': '𝙎', 'T': '𝙏', 'U': '𝙐', 'V': '𝙑', 'W': '𝙒', 'X': '𝙓', 'Y': '𝙔', 'Z': '𝙕',
 '0': '𝟎', '1': '𝟏', '2': '𝟐', '3': '𝟑', '4': '𝟒', '5': '𝟓', '6': '𝟔', '7': '𝟕', '8': '𝟖', '9': '𝟗'
-};
-return text.split('').map(char => map[char] || char).join('');
 }
-let handler = m => m
-handler.before = async function (m, { conn, groupMetadata }) {
-if (!m.messageStubType || !m.isGroup) return
-let chat = global.db.data.chats[m.chat]
-let usuario = m.sender.split('@')[0]
-let fkontak = null
-try {
-const res = await fetch('https://i.postimg.cc/6562JdR7/Hoshino-Ruby-(2).jpg')
-const thumb2 = await res.buffer()
-fkontak = {
-key: { participant: '0@s.whatsapp.net', remoteJid: 'status@broadcast', fromMe: false, id: 'Halo' },
-message: {
-locationMessage: {
-name: styleText('Notificaciones del Grupo'),
-jpegThumbnail: thumb2
+
+const styleText = (text) => text.split('').map((char) => STYLE_MAP[char] || char).join('')
+
+let detectThumbPromise = null
+async function getDetectThumb() {
+  if (!detectThumbPromise) {
+    detectThumbPromise = fetch('https://i.postimg.cc/6562JdR7/Hoshino-Ruby-(2).jpg')
+      .then((res) => (res.ok ? res.arrayBuffer() : null))
+      .then((buf) => (buf ? Buffer.from(buf) : null))
+      .catch(() => null)
+  }
+  return detectThumbPromise
 }
-},
-participant: '0@s.whatsapp.net'
-}
-} catch (e) {
-console.error(e)
-}
-let text = ''
-let mentions = [m.sender]
-if (chat.detect && m.messageStubType == 21) {
-text = `
+
+function buildDetectMessage(m, usuario) {
+  const stubType = m.messageStubType
+
+  if (stubType === 21) {
+    return {
+      text: `
        𖥔    　     *@${usuario}* ࣪      ˖ؚ
 ㅤだ ㅤׄㅤ *#* ㅤִㅤ✿ㅤׄ﹕ 𝐂𝐚𝐦𝐛𝐢𝐨́ 𝐞𝐥 𝐍𝐨𝐦𝐛𝐫𝐞   𖤝        
 ꒰꒰ 📝 𝐀𝐡𝐨𝐫𝐚 𝐬𝐞 𝐥𝐥𝐚𝐦𝐚 Ი꯭ᰍ
-> ${styleText(m.messageStubParameters[0])}`
-} else if (chat.detect && m.messageStubType == 22) {
-text = `
+> ${styleText(m.messageStubParameters?.[0] || '')}`,
+      mentions: [m.sender],
+    }
+  }
+
+  if (stubType === 22) {
+    return {
+      text: `
        𖥔    　     *@${usuario}* ࣪      ˖ؚ
 ㅤだ ㅤׄㅤ *#* ㅤִㅤ✿ㅤׄ﹕ 𝐂𝐚𝐦𝐛𝐢𝐨́ 𝐥𝐚 𝐈𝐦𝐚𝐠𝐞𝐧   𖤝        
 ꒰꒰ 🖼️ 𝐅𝐨𝐭𝐨 𝐀𝐜𝐭𝐮𝐚𝐥𝐢𝐳𝐚𝐝𝐚 Ი꯭ᰍ
-> 🫧 ${styleText('El icono del grupo ha cambiado')}`
-} else if (chat.detect && m.messageStubType == 24) {
-text = `
+> 🫧 ${styleText('El icono del grupo ha cambiado')}`,
+      mentions: [m.sender],
+    }
+  }
+
+  if (stubType === 24) {
+    return {
+      text: `
        𖥔    　     *@${usuario}* ࣪      ˖ؚ
 ㅤだ ㅤׄㅤ *#* ㅤִㅤ✿ㅤׄ﹕ 𝐂𝐚𝐦𝐛𝐢𝐨́ 𝐃𝐞𝐬𝐜𝐫𝐢𝐩𝐜𝐢𝐨́𝐧   𖤝        
 ꒰꒰ 📑 𝐈𝐧𝐟𝐨 𝐀𝐜𝐭𝐮𝐚𝐥𝐢𝐳𝐚𝐝𝐚 Ი꯭ᰍ
-> 📝 ${styleText('La descripción del grupo es nueva')}`
-} else if (chat.detect && m.messageStubType == 23) {
-text = `
+> 📝 ${styleText('La descripción del grupo es nueva')}`,
+      mentions: [m.sender],
+    }
+  }
+
+  if (stubType === 23) {
+    return {
+      text: `
        𖥔    　     *@${usuario}* ࣪      ˖ؚ
 ㅤだ ㅤׄㅤ *#* ㅤִㅤ✿ㅤׄ﹕ 𝐑𝐞𝐬𝐭𝐚𝐛𝐥𝐞𝐜𝐢𝐨́ 𝐄𝐧𝐥𝐚𝐜𝐞   𖤝        
 ꒰꒰ 🔗 𝐋𝐢𝐧𝐤 𝐀𝐧𝐮𝐥𝐚𝐝𝐨 Ი꯭ᰍ
-> 🚫 ${styleText('El enlace anterior ya no sirve')}`
-} else if (chat.detect && m.messageStubType == 25) {
-let type = m.messageStubParameters[0] == 'on' ? '𝐒𝐨𝐥𝐨 𝐀𝐝𝐦𝐢𝐧𝐬' : '𝐓𝐨𝐝𝐨𝐬'
-text = `
+> 🚫 ${styleText('El enlace anterior ya no sirve')}`,
+      mentions: [m.sender],
+    }
+  }
+
+  if (stubType === 25) {
+    const type = m.messageStubParameters?.[0] === 'on' ? '𝐒𝐨𝐥𝐨 𝐀𝐝𝐦𝐢𝐧𝐬' : '𝐓𝐨𝐝𝐨𝐬'
+    return {
+      text: `
        𖥔    　     *@${usuario}* ࣪      ˖ؚ
 ㅤだ ㅤׄㅤ *#* ㅤִㅤ✿ㅤׄ﹕ 𝐀𝐥𝐭𝐞𝐫𝐨́ 𝐀𝐣𝐮𝐬𝐭𝐞𝐬   𖤝        
 ꒰꒰ ⚙️ 𝐀𝐡𝐨𝐫𝐚 𝐄𝐝𝐢𝐭𝐚𝐧 Ი꯭ᰍ
-> 🔓 ${styleText(type)}`
-} else if (chat.detect && m.messageStubType == 26) {
-let action = m.messageStubParameters[0] == 'on' ? '𝐂𝐞𝐫𝐫𝐨́ 𝐞𝐥 𝐆𝐫𝐮𝐩𝐨' : '𝐀𝐛𝐫𝐢𝐨́ 𝐞𝐥 𝐆𝐫𝐮𝐩𝐨'
-let msg = m.messageStubParameters[0] == 'on' ? 'Solo Admins escriben' : 'Todos pueden escribir'
-text = `
+> 🔓 ${styleText(type)}`,
+      mentions: [m.sender],
+    }
+  }
+
+  if (stubType === 26) {
+    const closed = m.messageStubParameters?.[0] === 'on'
+    const action = closed ? '𝐂𝐞𝐫𝐫𝐨́ 𝐞𝐥 𝐆𝐫𝐮𝐩𝐨' : '𝐀𝐛𝐫𝐢𝐨́ 𝐞𝐥 𝐆𝐫𝐮𝐩𝐨'
+    const msg = closed ? 'Solo Admins escriben' : 'Todos pueden escribir'
+    return {
+      text: `
        𖥔    　     *@${usuario}* ࣪      ˖ؚ
 ㅤだ ㅤׄㅤ *#* ㅤִㅤ✿ㅤׄ﹕ ${action}   𖤝        
 ꒰꒰ 💬 𝐄𝐬𝐭𝐚𝐝𝐨 𝐝𝐞𝐥 𝐂𝐡𝐚𝐭 Ი꯭ᰍ
-> 📣 ${styleText(msg)}`
-} else if (chat.detect && m.messageStubType == 29) {
-let nuevoAdmin = m.messageStubParameters[0]
-mentions.push(nuevoAdmin)
-text = `
+> 📣 ${styleText(msg)}`,
+      mentions: [m.sender],
+    }
+  }
+
+  if (stubType === 29) {
+    const nuevoAdmin = m.messageStubParameters?.[0]
+    if (!nuevoAdmin) return null
+    return {
+      text: `
        𖥔    　     *@${usuario}* ࣪      ˖ؚ
 ㅤだ ㅤׄㅤ *#* ㅤִㅤ✿ㅤׄ﹕ 𝐃𝐢𝐨 𝐀𝐝𝐦𝐢𝐧 𝐚   𖤝        
 > 🫡 @${nuevoAdmin.split('@')[0]}
-꒰꒰ 👑 𝐍𝐮𝐞𝐯𝐨 𝐀𝐝𝐦𝐢𝐧 Ი꯭ᰍ`
-} else if (chat.detect && m.messageStubType == 30) {
-let exAdmin = m.messageStubParameters[0]
-mentions.push(exAdmin)
-text = `
+꒰꒰ 👑 𝐍𝐮𝐞𝐯𝐨 𝐀𝐝𝐦𝐢𝐧 Ი꯭ᰍ`,
+      mentions: [m.sender, nuevoAdmin],
+    }
+  }
+
+  if (stubType === 30) {
+    const exAdmin = m.messageStubParameters?.[0]
+    if (!exAdmin) return null
+    return {
+      text: `
        𖥔    　     *@${usuario}* ࣪      ˖ؚ
 ㅤだ ㅤׄㅤ *#* ㅤִㅤ✿ㅤׄ﹕ 𝐐𝐮𝐢𝐭𝐨́ 𝐀𝐝𝐦𝐢𝐧 𝐚   𖤝        
 > 😔 @${exAdmin.split('@')[0]}
-꒰꒰ 📉 𝐃𝐞𝐠𝐫𝐚𝐝𝐚𝐝𝐨 Ი꯭ᰍ`
+꒰꒰ 📉 𝐃𝐞𝐠𝐫𝐚𝐝𝐚𝐝𝐨 Ი꯭ᰍ`,
+      mentions: [m.sender, exAdmin],
+    }
+  }
+
+  return null
 }
-if (text) {
-await conn.sendMessage(m.chat, {
-text: text,
-mentions: mentions
-}, { quoted: fkontak || m })
+
+let handler = m => m
+handler.before = async function (m, { conn }) {
+  if (!m.messageStubType || !m.isGroup) return
+
+  const chat = global.db.data.chats[m.chat]
+  if (!chat.detect) return
+
+  const usuario = m.sender.split('@')[0]
+  const payload = buildDetectMessage(m, usuario)
+  if (!payload?.text) return
+
+  const thumb = await getDetectThumb()
+  const quoted = thumb
+    ? {
+      key: { participant: '0@s.whatsapp.net', remoteJid: 'status@broadcast', fromMe: false, id: 'Halo' },
+      message: {
+        locationMessage: {
+          name: styleText('Notificaciones del Grupo'),
+          jpegThumbnail: thumb,
+        },
+      },
+      participant: '0@s.whatsapp.net',
+    }
+    : m
+
+  await conn.sendMessage(m.chat, {
+    text: payload.text,
+    mentions: payload.mentions,
+  }, { quoted })
 }
-}
+
 export default handler
